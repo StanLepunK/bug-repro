35696d9aa211606b9cfe496f7102a7a43d90942e
Stanislas Marçais / Knupel <stanislasiii@gmail.com>
2022-01-21 12:46:29 +0100

    There is a content ful... hahahaha

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,18 @@
+require("dotenv").config();
 module.exports = {
     siteMetadata: {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+              spaceId: process.env.CONTENTFUL_SPACE_ID,
+              accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+              enableTags: true,
+            },
+          }
+        
 
-    ]
+    ],
 }
